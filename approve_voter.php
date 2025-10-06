781360cba39c816123fb117723a803cfc4aa6e0f
Tahmidro <161164487+Tahmidro@users.noreply.github.com>
2025-10-06 17:04:50 +0600

    Add files via upload

--- approve_voter.php
+++ approve_voter.php
@@ -6,10 +6,10 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-if (isset($_GET['user_id'])) {
-    $user_id = intval($_GET['user_id']);
-    $stmt = $conn->prepare("UPDATE voters SET is_verified=1 WHERE user_id=?");
-    $stmt->bind_param("i", $user_id);
+if (isset($_GET['voter_id'])) {
+    $voter_id = intval($_GET['voter_id']);
+    $stmt = $conn->prepare("UPDATE voters SET is_verified=1 WHERE voter_id=?");
+    $stmt->bind_param("i", $voter_id);
     $stmt->execute();
 }
 
