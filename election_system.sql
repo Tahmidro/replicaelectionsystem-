b383e06719777bc0370586aac57cc1731d523e1e
Tahmidro <161164487+Tahmidro@users.noreply.github.com>
2025-10-06 17:14:13 +0600

    Add files via upload

---
+++ election_system.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 06, 2025 at 01:13 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `election_system`
+--
+CREATE DATABASE IF NOT EXISTS `election_system` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `election_system`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidates`
+--
+
+DROP TABLE IF EXISTS `candidates`;
+CREATE TABLE IF NOT EXISTS `candidates` (
+  `candidate_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `status` enum('pending','approved','rejected') DEFAULT 'pending',
+  `election_id` int(11) DEFAULT NULL,
+  `party` varchar(100) DEFAULT NULL,
+  `manifesto` text DEFAULT NULL,
+  PRIMARY KEY (`candidate_id`),
+  KEY `user_id` (`user_id`),
+  KEY `fk_election` (`election_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `candidates`
+--
+
+INSERT INTO `candidates` (`candidate_id`, `user_id`, `status`, `election_id`, `party`, `manifesto`) VALUES(19, 55, 'rejected', NULL, 'bimpi', 'druto nirbachon');
+INSERT INTO `candidates` (`candidate_id`, `user_id`, `status`, `election_id`, `party`, `manifesto`) VALUES(20, 54, 'approved', 2, 'amlig', 'druto fire asbo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `elections`
+--
+
+DROP TABLE IF EXISTS `elections`;
+CREATE TABLE IF NOT EXISTS `elections` (
+  `election_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(150) NOT NULL,
+  `description` text DEFAULT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `status` enum('upcoming','ongoing','completed') DEFAULT 'upcoming',
+  PRIMARY KEY (`election_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `elections`
+--
+
+INSERT INTO `elections` (`election_id`, `title`, `description`, `start_time`, `end_time`, `status`) VALUES(1, 'national election', 'blah blah blah blah', '2025-10-02 00:00:00', '2025-10-06 00:00:00', 'upcoming');
+INSERT INTO `elections` (`election_id`, `title`, `description`, `start_time`, `end_time`, `status`) VALUES(2, 'chairman election', 'bllllllahh blahhh', '2025-10-05 00:00:00', '2025-10-06 00:00:00', 'upcoming');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `election_candidates`
+--
+
+DROP TABLE IF EXISTS `election_candidates`;
+CREATE TABLE IF NOT EXISTS `election_candidates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `election_id` int(11) NOT NULL,
+  `candidate_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `election_id` (`election_id`),
+  KEY `candidate_id` (`candidate_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `face_auth_logs`
+--
+
+DROP TABLE IF EXISTS `face_auth_logs`;
+CREATE TABLE IF NOT EXISTS `face_auth_logs` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `voter_id` int(11) DEFAULT NULL,
+  `nid` varchar(20) DEFAULT NULL,
+  `status` enum('success','failure') NOT NULL,
+  `attempt_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`log_id`),
+  KEY `voter_id` (`voter_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `nid` char(13) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_admin` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `nid` (`nid`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `email`, `password_hash`, `nid`, `created_at`, `is_admin`) VALUES(1, 'tanha', 'tanha@gmail.com', '$2y$10$pqGCyunge5tqDLs8yhsXbu80xM1JQ9Yu06tDTf04mzqlfR0GBbbF2', '0987654321234', '2025-09-28 13:53:01', 1);
+INSERT INTO `users` (`user_id`, `name`, `email`, `password_hash`, `nid`, `created_at`, `is_admin`) VALUES(54, 'opu', 'opu@gmail.com', '$2y$10$n5ZR/9oe8cxPubJpgeCkqe6bdYJZuOhDGJIhTKdpn/4x2ukd7WloW', '2222222222222', '2025-10-06 09:58:05', 0);
+INSERT INTO `users` (`user_id`, `name`, `email`, `password_hash`, `nid`, `created_at`, `is_admin`) VALUES(55, 'aniya', 'aniya@gmail.com', '$2y$10$0SjyUOf9xbx60bU/UK93.eddHt9OYiFg0GPuXFpz5qeSPWRruoSiu', '3333333333333', '2025-10-06 09:58:33', 0);
+INSERT INTO `users` (`user_id`, `name`, `email`, `password_hash`, `nid`, `created_at`, `is_admin`) VALUES(56, 'tahmid', 'tahmid@gmail.com', '$2y$10$XBeez23ZN5j55nXKHOvW..0oUpah5ycQHbfczt8m3HjkNhgjhaK3y', '7777777777777', '2025-10-06 10:49:31', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voters`
+--
+
+DROP TABLE IF EXISTS `voters`;
+CREATE TABLE IF NOT EXISTS `voters` (
+  `voter_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `nid_photo_path` varchar(255) DEFAULT NULL,
+  `self_photo_path` varchar(255) DEFAULT NULL,
+  `face_data_path` varchar(255) DEFAULT NULL,
+  `is_verified` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`voter_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `voters`
+--
+
+INSERT INTO `voters` (`voter_id`, `user_id`, `nid_photo_path`, `self_photo_path`, `face_data_path`, `is_verified`) VALUES(37, 54, 'uploads/voters/nid_54_Screenshot 2025-09-02 172510.png', 'uploads/voters/self_54_Screenshot 2025-09-02 172517.png', NULL, 1);
+INSERT INTO `voters` (`voter_id`, `user_id`, `nid_photo_path`, `self_photo_path`, `face_data_path`, `is_verified`) VALUES(38, 55, 'uploads/voters/nid_55_Screenshot 2025-09-02 172556.png', 'uploads/voters/self_55_Screenshot 2025-09-03 125322.png', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+DROP TABLE IF EXISTS `votes`;
+CREATE TABLE IF NOT EXISTS `votes` (
+  `vote_id` int(11) NOT NULL AUTO_INCREMENT,
+  `election_id` int(11) NOT NULL,
+  `voter_id` int(11) NOT NULL,
+  `candidate_id` int(11) NOT NULL,
+  `voted_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`vote_id`),
+  UNIQUE KEY `unique_vote` (`election_id`,`voter_id`),
+  KEY `voter_id` (`voter_id`),
+  KEY `candidate_id` (`candidate_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `candidates`
+--
+ALTER TABLE `candidates`
+  ADD CONSTRAINT `candidates_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_election` FOREIGN KEY (`election_id`) REFERENCES `elections` (`election_id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `election_candidates`
+--
+ALTER TABLE `election_candidates`
+  ADD CONSTRAINT `election_candidates_ibfk_1` FOREIGN KEY (`election_id`) REFERENCES `elections` (`election_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `election_candidates_ibfk_2` FOREIGN KEY (`candidate_id`) REFERENCES `candidates` (`candidate_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `face_auth_logs`
+--
+ALTER TABLE `face_auth_logs`
+  ADD CONSTRAINT `face_auth_logs_ibfk_1` FOREIGN KEY (`voter_id`) REFERENCES `voters` (`voter_id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `voters`
+--
+ALTER TABLE `voters`
+  ADD CONSTRAINT `voters_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `votes`
+--
+ALTER TABLE `votes`
+  ADD CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`election_id`) REFERENCES `elections` (`election_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`voter_id`) REFERENCES `voters` (`voter_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `votes_ibfk_3` FOREIGN KEY (`candidate_id`) REFERENCES `candidates` (`candidate_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
