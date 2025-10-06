61359d82b0e03b14a3d41bb4063fc980b930b9b1
Tahmidro <161164487+Tahmidro@users.noreply.github.com>
2025-10-06 17:08:42 +0600

    Add files via upload

---
+++ delete_account.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include "config.php";
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $stmt = $conn->prepare("delete from users WHERE user_id = ?");
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    session_destroy();
+}
+session_start();
+$_SESSION['flash_msg'] = "Account deleted successfully.";
+header("Location: index.php");
+exit;
+?>
