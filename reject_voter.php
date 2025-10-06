5feb53a4e102937ca79fc9e75656fc389ad36d47
Tahmidro <161164487+Tahmidro@users.noreply.github.com>
2025-10-06 17:12:02 +0600

    Add files via upload

--- reject_voter.php
+++ reject_voter.php
@@ -7,10 +7,10 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 
-if (isset($_GET['user_id'])) {
-    $user_id = intval($_GET['user_id']);
-    $stmt = $conn->prepare("DELETE FROM voters WHERE user_id=?");
-    $stmt->bind_param("i", $user_id);
+if (isset($_GET['voter_id'])) {
+    $voter_id = intval($_GET['voter_id']);
+    $stmt = $conn->prepare("DELETE FROM voters WHERE voter_id=?");
+    $stmt->bind_param("i", $voter_id);
     $stmt->execute();
 }
 
